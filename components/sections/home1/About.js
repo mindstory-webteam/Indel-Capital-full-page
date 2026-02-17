a23d95852e53bbc051423922fc512ccdf6a416a3
Ajith k v <ajith66310@gmail.com>
2026-02-17 13:46:32 +0530

    refactor: navbar

--- components/sections/home1/About.js
+++ components/sections/home1/About.js
@@ -1,65 +1,70 @@
 import Link from "next/link";
+import { AboutFeatures, aboutData } from '@/public/assets/assest.js';
 
 export default function About() {
-    return (
-        <>            
-            <section className="about-section pt_120 pb_120">
-                <div className="auto-container">
-                    <div className="row clearfix">
-                    <div className="col-lg-6 col-md-12 col-sm-12 image-column">
-                        <div className="image_block_one">
-                        <div className="image-box pr_90 mr_40">
-                            <figure className="image"><img src="assets/images/resource/about-1.jpg" alt="" /></figure>
-                            <div className="rating-box">
-                            <ul className="rating mb_5 clearfix">
-                                <li><i className="icon-9"></i></li>
-                                <li><i className="icon-9"></i></li>
-                                <li><i className="icon-9"></i></li>
-                                <li><i className="icon-9"></i></li>
-                                <li><i className="icon-9"></i></li>
-                            </ul>
-                            <h6>5 Star Rating Bank</h6>
-                            </div>
-                            <div className="experience-box">
-                            <div className="inner">
-                                <h2>40</h2>
-                                <h6>Years of Experience</h6>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
+  return (
+    <>
+      <section className="about-section pt_120 pb_120">
+        <div className="auto-container">
+          <div className="row clearfix">
+            <div className="col-lg-6 col-md-12 col-sm-12 image-column">
+              <div className="image_block_one">
+                <div className="image-box pr_90 mr_40">
+                  <figure className="image">
+                    <img src="assets/images/resource/about-1.jpg" alt="About Us" />
+                  </figure>
+                  <div className="rating-box">
+                    <ul className="rating mb_5 clearfix">
+                      {[...Array(5)].map((_, i) => (
+                        <li key={i}><i className="icon-9"></i></li>
+                      ))}
+                    </ul>
+                    <h6>{aboutData.ratingText}</h6>
+                  </div>
+                  <div className="experience-box">
+                    <div className="inner">
+                      <h2>{aboutData.experienceYears}</h2>
+                      <h6>Years of Experience</h6>
                     </div>
-                    <div className="col-lg-6 col-md-12 col-sm-12 content-column">
-                        <div className="content_block_one">
-                        <div className="content-box ml_40">
-                            <div className="sec-title mb_20">
-                            <h6>About US</h6>
-                            <h2>Financial Guidance for Every Stage of Life.</h2>
-                            </div>
-                            <div className="text-box mb_40">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis, suscipit you take action against fraud. See it the Security Center for and Mobile and Online Banking.</p>
-                            </div>
-                            <div className="inner-box mb_45">
-                            <div className="single-item">
-                                <div className="icon-box"><i className="icon-10"></i></div>
-                                <h3>Solution Focused</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis tincidunt feugiat</p>
-                            </div>
-                            <div className="single-item">
-                                <div className="icon-box"><i className="icon-11"></i></div>
-                                <h3>99.99% Success</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis tincidunt feugiat</p>
-                            </div>
-                            </div>
-                            <div className="btn-box">
-                            <Link href="/about" className="theme-btn btn-one">Discover More</Link>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+                  </div>
                 </div>
-            </section>
-        </>
-    )
+              </div>
+            </div>
+
+            <div className="col-lg-6 col-md-12 col-sm-12 content-column">
+              <div className="content_block_one">
+                <div className="content-box ml_40">
+                  <div className="sec-title mb_20">
+                    <h6>{aboutData.subTitle}</h6>
+                    <h2>{aboutData.mainTitle}</h2>
+                  </div>
+                  <div className="text-box mb_40">
+                    <p>{aboutData.description}</p>
+                  </div>
+                  
+                  <div className="inner-box mb_45">
+                    {AboutFeatures.map((item) => (
+                      <div className="single-item" key={item.id}>
+                        <div className="icon-box">
+                          <i className={item.icon}></i>
+                        </div>
+                        <h3>{item.title}</h3>
+                        <p>{item.description}</p>
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="btn-box">
+                    <Link href="/about" className="theme-btn btn-one">
+                      Discover More
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
---
+++ public/assets/assest.js
@@ -0,0 +1,22 @@
+export const AboutFeatures = [
+  {
+    id: 1,
+    icon: "icon-10",
+    title: "Solution Focused",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis tincidunt feugiat",
+  },
+  {
+    id: 2,
+    icon: "icon-11",
+    title: "99.99% Success",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis tincidunt feugiat",
+  },
+];
+
+export const aboutData = {
+  experienceYears: 40,
+  ratingText: "5 Star Rating Bank",
+  subTitle: "About US",
+  mainTitle: "Financial Guidance for Every Stage of Life.",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis, suscipit you take action against fraud. See it the Security Center for and Mobile and Online Banking.",
+};
